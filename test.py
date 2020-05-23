c2712ce8a499a42280a9e95d2050852c2f0a246b
Sasha Mayn <alexandra.mayn@gmail.com>
2020-05-23 18:14:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,5 @@ def testGetAllLocations():
 	url = "http://127.0.0.1:5000/api/v1/locations/alldb"
 	response = requests.get(url)
 	print(response.json())
-	# data = response.json()
 
 testGetAllLocations()
